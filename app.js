ad6834c9c9693c71d41007b362a0f443d1a62974
t-bear-git <60154712+t-bear-git@users.noreply.github.com>
2020-10-29 17:05:04 +0100

    second

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// Create element and assign it to variable
+// Created element and assigned variables
 const newButton = document.createElement('button');
 const firstWrapper = document.querySelector('.wrapper');
 const secondWrapper = document.querySelector('.wrapper2');
 const allWrappers = document.querySelectorAll('div');
-const allLiEl = document.querySelectorAll('li');
+const listElement = document.querySelector('ul');
 
 // Insert text in button
 newButton.innerHTML = 'Click Me!';
@@ -14,8 +14,10 @@ firstWrapper.appendChild(newButton);
 // Set class to first button
 newButton.className = 'first-button';
 
+// Event listener for button
 newButton.addEventListener('click', buttonFunction);
 
+// Functions to run on button click
 function buttonFunction() {
   newButton.insertAdjacentHTML(
     'afterend',
@@ -26,16 +28,14 @@ function buttonFunction() {
   console.log('Port: ', window.location.port);
   removeHandler();
 }
-
+// Function to remove eventlistener from button after click.
 function removeHandler() {
   newButton.removeEventListener('click', buttonFunction);
 }
 
+// Function to add/remove classes from allWrappers when page is scrolled >50px
 window.onscroll = () => {
-  if (
-    document.body.scrollTop > 150 ||
-    document.documentElement.scrollTop > 150
-  ) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     allWrappers[0].classList.add('div-borders');
     allWrappers[1].classList.add('div-borders');
   } else {
@@ -43,3 +43,43 @@ window.onscroll = () => {
     allWrappers[1].classList.remove('div-borders');
   }
 };
+
+// Array with info to be used
+let someFamily = [
+  { gender: 'Male', birthyear: 1957 },
+  { gender: 'Female', birthyear: 1959 },
+  { gender: 'Female', birthyear: 1983 },
+  { gender: 'Male', birthyear: 1987 },
+  { gender: 'Male', birthyear: 1987 },
+  { gender: 'Male', birthyear: 2007 },
+  { gender: 'Female', birthyear: 2011 },
+];
+
+// Array filter and map to create array to be shown on page
+let maleArr = someFamily.filter(familyMales).map(maleBirthyear);
+// Function to return array with only males
+function familyMales(x) {
+  return x.gender == 'Male';
+}
+// Function to return new array with only birthyear from above array
+function maleBirthyear(x) {
+  return x.birthyear;
+}
+
+// Function to create <li> for each item from filtered and mapped array
+function populateList() {
+  numberOfArrItems = maleArr.length;
+  for (i = 0; i < numberOfArrItems; ++i) {
+    // create an item for each one
+    listItem = document.createElement('li');
+
+    // Add the item text
+    listItem.innerHTML = maleArr[i];
+
+    // Add listItem to the listElement
+    listElement.appendChild(listItem);
+  }
+}
+
+// Using populateList function
+populateList();
